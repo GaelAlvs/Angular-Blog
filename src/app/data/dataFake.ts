eabdc4f817cf5a17f94bc2b801a702b7de5d0a9c
GaelAlvs <gabriel.alvs19@gmail.com>
2025-08-20 22:58:14 -0300

    deixando conteúdo dinâmico

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Novo filme lançado do Superman',
+    description:
+      'O universo DC imaginado do zero com uma mistura singular de ação épica, humor e emoção, apresentando um Superman que é movido pela compaixão e uma crença intrínseca na bondade do ser humano.',
+    photoCover:
+      'https://rollingstone.com.br/media/_versions/2025/08/superman-de-james-gunn-ganha-data-de-estreia-nas-plataformas-digitais_widemd.jpg',
+  },
+  {
+    id: '2',
+    title: 'The Boys lança nova temporada',
+    description:
+      'Nova temporada lançada de The Boys promete ser a última e fechar a série com chave de ouro.',
+    photoCover:
+      'https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/05/the-boys.jpeg?w=1200&h=1200&crop=1',
+  },
+  {
+    id: '3',
+    title: 'La Casa de Papel',
+    description:
+      'Na série La Casa de Papel, da Netflix, nove habilidosos ladrões se trancam na Casa da Moeda da Espanha, com o ambicioso plano de realizar o maior roubo da história.',
+    photoCover:
+      'https://cdn.ome.lt/E7yurjtgld2sbzA5bAbnkQJHLGY=/1200x630/smart/extras/conteudos/la-casa-head.jpg',
+  },
+  {
+    id: '4',
+    title: 'Stranger Things Temporada 5',
+    description:
+      'A 5ª e última temporada de Stranger Things estreará na Netflix em três partes, com o lançamento programado para o final de 2025, entre novembro e dezembro.',
+    photoCover:
+      'https://i.uai.com.br/WKg4tATNhXpMPtFNsBC8RttO9mc=/imgsapp2.uai.com.br/app/noticia_133890394703/2017/10/27/215831/20171027084328853028i.jpg',
+  },
+];
